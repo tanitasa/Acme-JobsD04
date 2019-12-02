44ef377cf8acf11f06c3c10b881d0e13a71d8295
Tania <tanitasa96@gmail.com>
2019-12-02 08:26:15 +0100

    Fix errors

--- src/main/java/acme/features/worker/application/WorkerApplicationRepository.java
+++ src/main/java/acme/features/worker/application/WorkerApplicationRepository.java
@@ -15,6 +15,6 @@ public interface WorkerApplicationRepository extends AbstractRepository {
 	@Query("select a from Application a where a.id = ?1")
 	Application findOneApplicationById(int id);
 
-	@Query("select a from Application a where a.worker = ?1")
+	@Query("select a from Application a where a.worker.id = ?1")
 	Collection<Application> findManyByWorkerId(int workerId);
 }
